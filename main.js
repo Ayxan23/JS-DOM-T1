43cadfe93ce9ed592b2af7209a46132cf4be4336
Ayxan23 <ayxan042223@gmail.com>
2023-02-16 02:51:10 +0400

    23

--- main.js
+++ main.js
@@ -7,22 +7,10 @@ function Car(brand, model, year, descr, isNew) {
   this.isNew = isNew;
 }
 
-/*New checkbox
-const is_new = document.getElementById("is_new");
-
-let box_res = false;
-const checkBox = () => {
-  if (!box_res) {
-    box_res = true;
-  } else {
-    box_res = false;
-  }
-};
-is_new.addEventListener("click", checkBox);*/
-
-//ADD button
+//Select tags
 const btn_sub = document.querySelector(".btn_sub");
-let resultArr = [];
+const table = document.querySelector(".table");
+const exp_text = document.querySelector(".exp_text");
 
 let brand = document.getElementById("brand");
 let model = document.getElementById("model");
@@ -30,114 +18,59 @@ let year = document.getElementById("year");
 let descr = document.getElementById("descr");
 let is_new = document.getElementById("is_new");
 
-const AddCar = () => {
-  const exp_text = document.querySelector(".exp_text");
-  if (
-    brand.value != "" &&
-    model.value != "" &&
-    year.value != "" &&
-    descr.value != ""
-  ) {
-    const newCar = new Car(
-      brand.value,
-      model.value,
-      Number(year.value),
-      descr.value,
-      is_new.checked
-    );
+//Creat some arrays
+let arrInput = [brand, model, year, descr];
+let resultArr = [];
+
+//Add Button Listener
+btn_sub.addEventListener("click", () => {
+  //let arrValues = arrInput.map((item)=> item.value);
+  let arrValues = [brand.value, model.value, year.value, descr.value];
+
+  if (arrValues.every((item) => item != "")) {
+    //Creat Car Obj
+    const newCar = new Car(...arrValues, is_new.checked);
     resultArr.push(newCar);
-    exp_text.innerHTML = "";
-    brand.value = "";
-    model.value = "";
-    year.value = "";
-    descr.value = "";
+
+    //Reset Values
+    exp_text.innerHTML = null;
+    arrInput.forEach((item) => (item.value = null));
     is_new.checked = false;
   } else {
-    exp_text.innerHTML = "Bos qoymaq olmaz!";
+    //Empty error feedBack
+    exp_text.innerHTML = "Please fill all the fields!";
 
-    if (brand.value == "") {
-      brand.style.backgroundColor = "#ffd3d3";
-      brand.style.border = "2px solid rgb(255, 88, 88)";
-    }
-    if (model.value == "") {
-      model.style.backgroundColor = "#ffd3d3";
-      model.style.border = "2px solid rgb(255, 88, 88)";
-    }
-    if (year.value == "") {
-      year.style.backgroundColor = "#ffd3d3";
-      year.style.border = "2px solid rgb(255, 88, 88)";
-    }
-    if (descr.value == "") {
-      descr.style.backgroundColor = "#ffd3d3";
-      descr.style.border = "2px solid rgb(255, 88, 88)";
-    }
+    arrInput.forEach((box) => {
+      if (box.value == "") {
+        box.style.backgroundColor = "#ffd3d3";
+        box.style.border = "2px solid rgb(255, 88, 88)";
+      }
+    });
     return;
   }
-  //console.log(resultArr);
 
-  //Creat Table
-  resultArr.forEach((item) => {
-    resBrand = item.brand;
-    resModel = item.model;
-    resYear = String(item.year);
-    resDescr = item.descr;
-    if (item.isNew) {
-      item.isNew = "Yes";
-    } else {
-      item.isNew = "No";
-    }
-    resIsNew = item.isNew;
+  //Add Table Row
+  resultArr.forEach((obj) => {
+    obj.isNew ? (obj.isNew = "Yes") : (obj.isNew = "No");
+
     let tr_new = document.createElement("tr");
     table.append(tr_new);
 
-    let td1 = document.createElement("td");
-    tr_new.append(td1);
-    td1.innerHTML = resBrand;
+    let arrItem = [obj.brand, obj.model, obj.year, obj.descr, obj.isNew];
 
-    let td2 = document.createElement("td");
-    tr_new.append(td2);
-    td2.innerHTML = resModel;
-
-    let td3 = document.createElement("td");
-    tr_new.append(td3);
-    td3.innerHTML = resYear;
-
-    let td4 = document.createElement("td");
-    tr_new.append(td4);
-    td4.innerHTML = resDescr;
-
-    let td5 = document.createElement("td");
-    tr_new.append(td5);
-    td5.innerHTML = resIsNew;
+    arrItem.forEach((td_item) => {
+      let td_new = document.createElement("td");
+      tr_new.append(td_new);
+      td_new.innerHTML = td_item;
+    });
   });
   resultArr = [];
-};
-btn_sub.addEventListener("click", AddCar);
-
-//Add Element
-const table = document.querySelector(".table");
+});
 
 //Red color to default
-const colorBrand = () => {
-  brand.style.backgroundColor = null;
-  brand.style.border = null;
-};
-brand.addEventListener("click", colorBrand);
-
-const colorModel = () => {
-  model.style.backgroundColor = null;
-  model.style.border = null;
-};
-model.addEventListener("click", colorModel);
-
-const colorYear = () => {
-  year.style.backgroundColor = null;
-  year.style.border = null;
-};
-year.addEventListener("click", colorYear);
-
-const colorDescr = () => {
-  descr.style.backgroundColor = null;
-  descr.style.border = null;
-};
-descr.addEventListener("click", colorDescr);
+arrInput.forEach((box) => {
+  box.addEventListener("click", () => {
+    box.style.backgroundColor = null;
+    box.style.border = null;
+  });
+});
